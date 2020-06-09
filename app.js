d5c2a116f9729d33e188effca2a6893524f4b444
atomicwelding <ctfweld@gmail.com>
2020-06-10 00:01:04 +0200

    refactor

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var express = require("express");
 var App = /** @class */ (function () {
     function App(port) {
         this.port = port;
-        this.passwd = '####';
-        this.salt = '####';
+        this.passwd = '***';
+        this.salt = '***';
     }
     App.getInstance = function (port) {
         if (!App.instance)
@@ -22,68 +22,76 @@ var App = /** @class */ (function () {
         return App.instance;
     };
     /**
+     * Methods used in the router
+     */
+    App.prototype.gallery_get = function (req, res) {
+        var tmpfd = fs.readFileSync(path.join(__dirname + '/www/gallery.html'));
+        var dom = new jsdom.JSDOM(tmpfd);
+        //create the ul, append it to the div
+        var node = dom.window.document.createElement('ul');
+        node.id = 'list';
+        dom.window.document.querySelector('#gallery').appendChild(node);
+        // take the list of files from the directory /www/media/
+        var list = fs.readdirSync(path.join(__dirname + '/www/media/'));
+        // sort by date, newest first
+        list.sort(function (a, b) { return fs.statSync(path.join(__dirname + '/www/media/' + b)).mtime.getTime() - fs.statSync(path.join(__dirname + '/www/media/' + a)).mtime.getTime(); });
+        list.forEach(function (file) {
+            var tmpdate = new Date();
+            var n = dom.window.document.createElement('li');
+            n.innerHTML = '<a href="/media/' + file + '">' + file + '</a>';
+            dom.window.document.querySelector('#list').appendChild(n);
+        });
+        res.send(dom.serialize());
+    };
+    App.prototype.rsrc_get = function (req, res) {
+        var filepath = path.join(__dirname + '/www/rsrc/' + req.params[0]);
+        if (!fs.existsSync(filepath))
+            res.sendFile(path.join(__dirname + '/www/404.html'));
+        else
+            res.sendFile(filepath);
+    };
+    App.prototype.upload_get = function (req, res) {
+        var filepath = path.join(__dirname + '/www/media/' + req.params[0]);
+        if (!fs.existsSync(filepath))
+            res.sendFile(path.join(__dirname + '/www/404.html'));
+        else
+            res.sendFile(filepath);
+    };
+    App.prototype.upload_post = function (req, res) {
+        var _this = this;
+        var f = form({ multiples: true });
+        f.parse(req, function (err, fields, file) {
+            if (err)
+                console.log(err);
+            else if (fields.passwd = _this.passwd) {
+                file = file[Object.keys(file)[0]];
+                var authorized_extensions = ['jpg', 'jpeg', 'png', 'gif', 'txt'];
+                if (authorized_extensions.includes(mime.getExtension(file.type))) {
+                    var filepath = path.join(__dirname + '/www/media/' + md5(Date.now() + _this.salt) + '.' + mime.getExtension(file.type));
+                    fs.createReadStream(file.path).pipe(fs.createWriteStream(filepath));
+                    res.send('<script>alert("file uploaded")</script>');
+                }
+                else
+                    res.send('Not authorized');
+            }
+        });
+    };
+    /**
      * Here is the main ! The router is implemented differently for each allowed routes
      */
     App.prototype.start = function () {
         var _this = this;
         var router = express();
-        // main page
+        // main page of the website
         router.get('/', function (req, res) { res.sendFile(path.join(__dirname + '/www/index.html')); });
-        // css, js, images, ... for the website
-        router.get('/rsrc/*', function (req, res) {
-            var filepath = path.join(__dirname + '/www/rsrc/' + req.params[0]);
-            if (!fs.existsSync(filepath))
-                res.sendFile(path.join(__dirname + '/www/404.html'));
-            else
-                res.sendFile(filepath);
-        });
+        // here are the needed resources to the website (css, js, images, ...)
+        router.get('/rsrc/*', function (req, res) { return _this.rsrc_get(req, res); });
         // main page of the gallery
-        router.get('/gallery/?', function (req, res) {
-            var tmpfd = fs.readFileSync(path.join(__dirname + '/www/gallery.html'));
-            var dom = new jsdom.JSDOM(tmpfd);
-            //create the ul, append it to the div
-            var node = dom.window.document.createElement('ul');
-            node.id = 'list';
-            dom.window.document.querySelector('#gallery').appendChild(node);
-            // take the list of files from the directory /www/media/
-            var list = fs.readdirSync(path.join(__dirname + '/www/media/'));
-            // sort by date, newest first
-            list.sort(function (a, b) { return fs.statSync(path.join(__dirname + '/www/media/' + b)).mtime.getTime() - fs.statSync(path.join(__dirname + '/www/media/' + a)).mtime.getTime(); });
-            list.forEach(function (file) {
-                var tmpdate = new Date();
-                var n = dom.window.document.createElement('li');
-                n.innerHTML = '<a href="/media/' + file + '">' + file + '</a>';
-                dom.window.document.querySelector('#list').appendChild(n);
-            });
-            res.send(dom.serialize());
-        });
-        // files uploaded
-        router.get('/media/*', function (req, res) {
-            var filepath = path.join(__dirname + '/www/media/' + req.params[0]);
-            if (!fs.existsSync(filepath))
-                res.sendFile(path.join(__dirname + '/www/404.html'));
-            else
-                res.sendFile(filepath);
-        });
-        // uploader
-        router.post('/upload/?', function (req, res) {
-            var f = form({ multiples: true });
-            f.parse(req, function (err, fields, file) {
-                if (err)
-                    console.log(err);
-                else if (fields.passwd = _this.passwd) {
-                    file = file[Object.keys(file)[0]];
-                    var authorized_extensions = ['jpg', 'jpeg', 'png', 'gif', 'txt'];
-                    if (authorized_extensions.includes(mime.getExtension(file.type))) {
-                        var filepath = path.join(__dirname + '/www/media/' + md5(Date.now() + _this.salt) + '.' + mime.getExtension(file.type));
-                        fs.createReadStream(file.path).pipe(fs.createWriteStream(filepath));
-                        res.send('<script>alert("file uploaded")</script>');
-                    }
-                    else
-                        res.send('Not authorized');
-                }
-            });
-        });
+        router.get('/gallery/?', function (req, res) { return _this.gallery_get(req, res); });
+        // file uploaded are provided by this function
+        router.get('/media/*', function (req, res) { return _this.upload_get(req, res); });
+        // uploading a file
+        router.post('/upload/?', function (req, res) { return _this.upload_post(req, res); });
         router.listen(this.port, function () { return console.log('server is now listening'); });
     };
     return App;
--- app.ts
+++ app.ts
@@ -16,8 +16,8 @@ class App {
     private salt: string;
     private constructor(port: number) {
         this.port = port;
-        this.passwd = '####';
-        this.salt = '####';
+        this.passwd = '***';
+        this.salt = '***';
     }
 
     /**
@@ -30,63 +30,51 @@ class App {
         return App.instance;
     }
 
+
     /**
-     * Here is the main ! The router is implemented differently for each allowed routes
+     * Methods used in the router
      */
-    public start(): void {
-        const router = express();
+    private gallery_get(req, res): void {
+        const tmpfd = fs.readFileSync(path.join(__dirname + '/www/gallery.html'));
+        const dom = new jsdom.JSDOM(tmpfd);
+        //create the ul, append it to the div
+        const node = dom.window.document.createElement('ul');
+        node.id = 'list';
+        dom.window.document.querySelector('#gallery').appendChild(node);
+     
+        // take the list of files from the directory /www/media/
+        const list = fs.readdirSync(path.join(__dirname + '/www/media/'));
 
-        // main page
-        router.get('/', (req, res) => {res.sendFile(path.join(__dirname + '/www/index.html'));});
-        
-        // css, js, images, ... for the website
-        router.get('/rsrc/*', (req, res) => {
-            
-            const filepath: string = path.join(__dirname + '/www/rsrc/'+req.params[0]);
-            if(!fs.existsSync(filepath))
-                res.sendFile(path.join(__dirname + '/www/404.html'));
-            else
-                res.sendFile(filepath);
-        });
-        
-        // main page of the gallery
-        router.get('/gallery/?', (req, res) => {
-            
-            const tmpfd = fs.readFileSync(path.join(__dirname + '/www/gallery.html'));
-            const dom = new jsdom.JSDOM(tmpfd);
+        // sort by date, newest first
+        list.sort((a,b) => fs.statSync(path.join(__dirname + '/www/media/' + b)).mtime.getTime() - fs.statSync(path.join(__dirname + '/www/media/' + a)).mtime.getTime());
+        list.forEach(file => {
+            let tmpdate = new Date();
+            const n = dom.window.document.createElement('li');
+            n.innerHTML = '<a href="/media/'+file+'">'+file+'</a>';
+            dom.window.document.querySelector('#list').appendChild(n);
+        })
 
-            //create the ul, append it to the div
-            const node = dom.window.document.createElement('ul');
-            node.id = 'list';
-            dom.window.document.querySelector('#gallery').appendChild(node);
-         
-            // take the list of files from the directory /www/media/
-            const list = fs.readdirSync(path.join(__dirname + '/www/media/'));
+        res.send(dom.serialize());
+    }
 
-            // sort by date, newest first
-            list.sort((a,b) => fs.statSync(path.join(__dirname + '/www/media/' + b)).mtime.getTime() - fs.statSync(path.join(__dirname + '/www/media/' + a)).mtime.getTime());
-            list.forEach(file => {
-                let tmpdate = new Date();
-                const n = dom.window.document.createElement('li');
-                n.innerHTML = '<a href="/media/'+file+'">'+file+'</a>';
-                dom.window.document.querySelector('#list').appendChild(n);
-            })
+    private rsrc_get(req, res): void {
+        const filepath: string = path.join(__dirname + '/www/rsrc/'+req.params[0]);
+        if(!fs.existsSync(filepath))
+            res.sendFile(path.join(__dirname + '/www/404.html'));
+        else
+            res.sendFile(filepath);
+    }
 
-            res.send(dom.serialize());
-        });
+    private upload_get(req, res): void {
+        const filepath: string = path.join(__dirname + '/www/media/'+req.params[0]);
+        if(!fs.existsSync(filepath))
+            res.sendFile(path.join(__dirname + '/www/404.html'));
+        else
+            res.sendFile(filepath);
+    }
 
-        // files uploaded
-        router.get('/media/*', (req, res) => {
-            const filepath: string = path.join(__dirname + '/www/media/'+req.params[0]);
-            if(!fs.existsSync(filepath))
-                res.sendFile(path.join(__dirname + '/www/404.html'));
-            else
-                res.sendFile(filepath);
-        });
-
-        // uploader
-        router.post('/upload/?', (req, res) => {
-            const f = form({multiples:true});
+    private upload_post(req, res): void {
+        const f = form({multiples:true});
             f.parse(req, (err, fields, file) => {
                 if(err)
                     console.log(err)
@@ -103,7 +91,23 @@ class App {
                         res.send('Not authorized');
                 }
             });
-        });
+    }
+
+    /**
+     * Here is the main ! The router is implemented differently for each allowed routes
+     */
+    public start(): void {
+        const router = express();
+        // main page of the website
+        router.get('/', (req, res) => {res.sendFile(path.join(__dirname + '/www/index.html'));});
+        // here are the needed resources to the website (css, js, images, ...)
+        router.get('/rsrc/*', (req, res) => this.rsrc_get(req, res));
+        // main page of the gallery
+        router.get('/gallery/?', (req, res) => this.gallery_get(req, res));
+        // file uploaded are provided by this function
+        router.get('/media/*', (req, res) => this.upload_get(req, res));
+        // uploading a file
+        router.post('/upload/?', (req, res) => this.upload_post(req, res));
 
         router.listen(this.port, () => console.log('server is now listening'));
     }
